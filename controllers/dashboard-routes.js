0cd671db0e3b1f9f58cb2c6cf91151d283cb2d4a
Alex <lex.courtney18@gmail.com>
2022-08-29 12:37:01 -0500

    add style to delete post button and fix bug

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -39,7 +39,7 @@ router.get('/', withAuth, (req, res) => {
         });
 });
 
-router.get('edit/:id', withAuth, (req, res) => {
+router.get('/edit/:id', withAuth, (req, res) => {
     Post.findOne({
         where: {
             id: req.params.id
@@ -53,7 +53,7 @@ router.get('edit/:id', withAuth, (req, res) => {
         include: [
             {
                 model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'crated_at'],
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
                 include: {
                     model: User,
                     attributes: ['username']
